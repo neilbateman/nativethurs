e92342695b631a9ff8b046948a29ada1cd84fd2e
Student <example@example.com>
2019-10-07 10:08:48 -0700

    morning commit, firebase entries persisting and displaying

---
+++ App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import BoardScreen from './components/BoardScreen';
+import BoardDetailScreen from './components/BoardDetailScreen';
+import AddBoardScreen from './components/AddBoardScreen';
+import EditBoardScreen from './components/EditBoardScreen';
+
+const Stack = createStackNavigator(
+  {
+    Board: BoardScreen,
+    BoardDetails: BoardDetailScreen,
+    AddBoard: AddBoardScreen,
+    EditBoard: EditBoardScreen,
+  },
+  {
+    initialRouteName: 'Board',
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#777777',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+      headerBackTitle: null,
+    },
+  },
+);
+
+const RootStack = createAppContainer(Stack);
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ Firebase.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase';
+import firestore from 'firebase/firestore'
+
+const settings = {timestampsInSnapshots: true};
+
+const config = {
+  apiKey: "AIzaSyD50OdUja6YXuO5p_ly2f6e7ostR3aa6k4",
+    authDomain: "native-5e08a.firebaseapp.com",
+    databaseURL: "https://native-5e08a.firebaseio.com",
+    projectId: "native-5e08a",
+    storageBucket: "native-5e08a.appspot.com",
+    messagingSenderId: "555877354800",
+    appId: "1:555877354800:web:d6fdb4c472db4adc4accf5",
+    measurementId: "G-3WBVB4D3Q8"
+};
+firebase.initializeApp(config);
+
+firebase.firestore().settings(settings);
+
+export default firebase;
